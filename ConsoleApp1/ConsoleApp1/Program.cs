dbf11f5f5dc026ba12d27abeb96e0473788b4201
Sirada Soonthorn <ploy_puth@outlook.co.th>
2019-10-24 22:19:48 +0700

    lap

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object o2 = null;
+            int i2 = (int)o2; // Error
+        }
+    }
+}
